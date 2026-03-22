15e134c91a1c5b028dc54a8e6c3c654b3021196d
Kareena2070 <kareenayadav1088@gmail.com>
2026-03-22 23:43:12 +0530

    feat: integrate AI interview report generation with schema and test data:- Added generateInterviewReport service using Gemini API - Defined Zod schema for structured AI response validation - Created Mongoose schemas for interview report storage - Added temporary sample data (resume, jobDescription, selfDescription) - Trigger AI report generation on server start for testing

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,12 @@
 require("dotenv").config()
 const app = require("./src/app")
 const connectDB = require("./src/config/db.js")
+const {resume, jobDescription, selfDescription} = require("./src/services/temp.js")
+const generateInterviewReport = require("./src/services/ai.service.js")
 
 connectDB()
+generateInterviewReport({resume, jobDescription, selfDescription})
+
 
 app.listen(3000, ()=>{
     console.log("server is running on 3000 port")
